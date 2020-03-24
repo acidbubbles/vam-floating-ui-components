e40bdbe6d9fdb98885bb5a5b461cd337e599ccf7
Acid Bubbles <acidbubbles.anon@gmail.com>
2020-03-24 00:01:13 -0400

    Simply for easier reuse

--- UISlider.cs
+++ UISlider.cs
@@ -16,22 +16,22 @@ public class UISlider : MVRScript
     private JSONStorableFloat _valueJSON;
     private JSONStorableStringChooser _atomJSON;
     private JSONStorableStringChooser _storableJSON;
-    private JSONStorableStringChooser _floatParamJSON;
+    private JSONStorableStringChooser _storableParamJSON;
     private Atom _atom;
-    private Transform _sliderTransform;
-    private JSONStorableFloat _targetFloatParam;
-    private UIDynamicSlider _sliderUI;
+    private JSONStorableFloat _targetParam;
+    private Transform _uiTransform;
+    private UIDynamicSlider _ui;
 
     public override void Init()
     {
-        _atom = GetAtom();
-
         try
         {
+            _atom = GetAtom();
+
             _labelJSON = new JSONStorableString("Label", "My Slider", label =>
             {
-                if (_sliderUI == null) return;
-                _sliderUI.label = label;
+                if (_ui == null) return;
+                _ui.label = label;
             });
             RegisterString(_labelJSON);
             CreateTextInput(_labelJSON);
@@ -42,7 +42,6 @@ public class UISlider : MVRScript
             OnEnable();
 
             _atomJSON = new JSONStorableStringChooser("Target atom", SuperController.singleton.GetAtomUIDs(), "", "Target atom", uid => OnTargetAtomChanged(uid));
-            _atomJSON.storeType = JSONStorableParam.StoreType.Physical;
             RegisterStringChooser(_atomJSON);
             var atomPopup = CreateScrollablePopup(_atomJSON, true);
             atomPopup.popupPanelHeight = 800f;
@@ -51,21 +50,19 @@ public class UISlider : MVRScript
 
             var storables = new List<string>(new[] { "" });
             _storableJSON = new JSONStorableStringChooser("Target storable", storables, "", "Target storable", storable => OnTargetStorableChanged(storable));
-            _storableJSON.storeType = JSONStorableParam.StoreType.Physical;
             RegisterStringChooser(_storableJSON);
             var storablePopup = CreateScrollablePopup(_storableJSON, true);
             storablePopup.popupPanelHeight = 800f;
 
-            var floatParams = new List<string>(new[] { "" });
-            _floatParamJSON = new JSONStorableStringChooser("Target param", storables, "", "Target param", floatParam => OnTargetFloatParamChanged(floatParam));
-            _floatParamJSON.storeType = JSONStorableParam.StoreType.Physical;
-            RegisterStringChooser(_floatParamJSON);
-            var floatParamPopup = CreateScrollablePopup(_floatParamJSON, true);
-            floatParamPopup.popupPanelHeight = 800f;
+            var storableParams = new List<string>(new[] { "" });
+            _storableParamJSON = new JSONStorableStringChooser("Target param", storables, "", "Target param", floatParam => OnTargetParamChanged(floatParam));
+            RegisterStringChooser(_storableParamJSON);
+            var storableParamUI = CreateScrollablePopup(_storableParamJSON, true);
+            storableParamUI.popupPanelHeight = 800f;
         }
         catch (Exception exc)
         {
-            SuperController.LogError("UISlider Enable: " + exc);
+            SuperController.LogError($"{nameof(UISlider)}.{nameof(Init)}: " + exc);
         }
     }
 
@@ -73,13 +70,10 @@ public class UISlider : MVRScript
     {
         // Note: Yeah, that's horrible, but containingAtom is null
         var container = gameObject?.transform?.parent?.parent?.parent?.parent?.parent?.gameObject;
-        if (container == null)
-            throw LogError(new NullReferenceException($"UISlider could not find the parent gameObject"));
+        if (container == null) throw new NullReferenceException($"{nameof(UISlider)} could not find the parent gameObject");
         var atom = container.GetComponent<Atom>();
-        if (atom == null)
-            throw LogError(new NullReferenceException($"UISlider could not find the parent atom in {container.name}"));
-        if (atom.type != "SimpleSign")
-            throw LogError(new InvalidOperationException("UISlider can only be applied on SimpleSign"));
+        if (atom == null) throw new NullReferenceException($"{nameof(UISlider)} could not find the parent atom in {container.name}");
+        if (atom.type != "SimpleSign") throw new InvalidOperationException($"{nameof(UISlider)} can only be applied on SimpleSign");
         return atom;
     }
 
@@ -93,7 +87,7 @@ public class UISlider : MVRScript
         }
         catch (Exception exc)
         {
-            SuperController.LogError("UISlider OnAtomsChanged: " + exc);
+            SuperController.LogError($"{nameof(UISlider)}.{nameof(OnAtomsChanged)}: " + exc);
         }
     }
 
@@ -109,7 +103,7 @@ public class UISlider : MVRScript
             }
 
             var atom = SuperController.singleton.GetAtomByUid(uid);
-            if (atom == null) throw LogError(new NullReferenceException($"Atom {uid} does not exist"));
+            if (atom == null) throw new NullReferenceException($"Atom {uid} does not exist");
             var storables = new List<string>(atom.GetStorableIDs());
             storables.Insert(0, "");
             _storableJSON.choices = storables;
@@ -117,7 +111,7 @@ public class UISlider : MVRScript
         }
         catch (Exception exc)
         {
-            SuperController.LogError("UISlider OnTargetAtomChanged: " + exc);
+            SuperController.LogError($"{nameof(UISlider)}.{nameof(OnTargetAtomChanged)}: " + exc);
         }
     }
 
@@ -127,105 +121,99 @@ public class UISlider : MVRScript
         {
             if (sid == "")
             {
-                _floatParamJSON.choices = new List<string>(new[] { "" });
-                _floatParamJSON.val = "";
+                _storableParamJSON.choices = new List<string>(new[] { "" });
+                _storableParamJSON.val = "";
                 return;
             }
 
             if (_atomJSON.val == "") return;
 
             var atom = SuperController.singleton.GetAtomByUid(_atomJSON.val);
-            if (atom == null) throw LogError(new NullReferenceException($"Atom {_atomJSON.val} does not exist"));
+            if (atom == null) throw new NullReferenceException($"Atom {_atomJSON.val} does not exist");
             var storable = atom.GetStorableByID(sid);
-            if (storable == null) throw LogError(new NullReferenceException($"Storable {sid} of atom {_atomJSON.val} does not exist"));
+            if (storable == null) throw new NullReferenceException($"Storable {sid} of atom {_atomJSON.val} does not exist");
             var floatParams = new List<string>(storable.GetFloatParamNames());
             floatParams.Insert(0, "");
-            _floatParamJSON.choices = floatParams;
-            _floatParamJSON.val = floatParamNames.FirstOrDefault(s => s == _floatParamJSON.val) ?? "";
+            _storableParamJSON.choices = floatParams;
+            _storableParamJSON.val = floatParamNames.FirstOrDefault(s => s == _storableParamJSON.val) ?? "";
         }
         catch (Exception exc)
         {
-            SuperController.LogError("UISlider OnTargetAtomChanged: " + exc);
+            SuperController.LogError($"{nameof(UISlider)}.{nameof(OnTargetStorableChanged)}: " + exc);
         }
     }
 
-    private void OnTargetFloatParamChanged(string floatParamName)
+    private void OnTargetParamChanged(string paramName)
     {
 
         if (_atomJSON.val == "") return;
         if (_storableJSON.val == "") return;
-        if (floatParamName == "") return;
-
+        if (paramName == "") return;
 
         var atom = SuperController.singleton.GetAtomByUid(_atomJSON.val);
-        if (atom == null) throw LogError(new NullReferenceException($"Atom {_atomJSON.val} does not exist"));
+        if (atom == null) throw new NullReferenceException($"Atom {_atomJSON.val} does not exist");
         var storable = atom.GetStorableByID(_storableJSON.val);
-        if (storable == null) throw LogError(new NullReferenceException($"Storable {_storableJSON.val} of atom {_atomJSON.val} does not exist"));
-        _targetFloatParam = storable.GetFloatJSONParam(floatParamName);
-        if (_targetFloatParam == null) throw LogError(new NullReferenceException($"Float JSON param {floatParamName} of storable {_storableJSON.val} of atom {_atomJSON.val} does not exist"));
+        if (storable == null) throw new NullReferenceException($"Storable {_storableJSON.val} of atom {_atomJSON.val} does not exist");
+        _targetParam = storable.GetFloatJSONParam(paramName);
+        if (_targetParam == null) throw new NullReferenceException($"Float JSON param {paramName} of storable {_storableJSON.val} of atom {_atomJSON.val} does not exist");
 
         _valueJSON.constrained = false;
-        _valueJSON.defaultVal = _targetFloatParam.defaultVal;
-        _valueJSON.val = _targetFloatParam.val;
-        _valueJSON.min = _targetFloatParam.min;
-        _valueJSON.max = _targetFloatParam.max;
-        _valueJSON.constrained = _targetFloatParam.constrained;
-        _sliderUI.Configure(_labelJSON.val, _valueJSON.min, _valueJSON.max, _valueJSON.defaultVal, _valueJSON.constrained, "F2", true, !_valueJSON.constrained);
+        _valueJSON.defaultVal = _targetParam.defaultVal;
+        _valueJSON.val = _targetParam.val;
+        _valueJSON.min = _targetParam.min;
+        _valueJSON.max = _targetParam.max;
+        _valueJSON.constrained = _targetParam.constrained;
+        _ui.Configure(_labelJSON.val, _valueJSON.min, _valueJSON.max, _valueJSON.defaultVal, _valueJSON.constrained, "F2", true, !_valueJSON.constrained);
     }
 
     private void UpdateValue(float v)
     {
-        if (_targetFloatParam == null) return;
-        _targetFloatParam.val = v;
+        if (_targetParam == null) return;
+        _targetParam.val = v;
     }
 
     public void OnEnable()
     {
-        if (_sliderTransform != null || _atom == null) return;
+        if (_uiTransform != null || _atom == null) return;
 
         try
         {
-            CreateUISliderInCanvas();
+            var canvas = _atom.GetComponentInChildren<Canvas>();
+            if (canvas == null) throw new NullReferenceException("Could not find a canvas to attach to");
+
+            _uiTransform = Instantiate(manager.configurableSliderPrefab.transform);
+            if (_uiTransform == null) throw new NullReferenceException("Could not instantiate configurableSliderPrefab");
+            _uiTransform.SetParent(canvas.transform, false);
+            _uiTransform.gameObject.SetActive(true);
+
+            _ui = _uiTransform.GetComponent<UIDynamicSlider>();
+            if (_ui == null) throw new NullReferenceException("Could not find a UIDynamicSlider component");
+            _ui.Configure(_labelJSON.val, _valueJSON.min, _valueJSON.max, _valueJSON.val, _valueJSON.constrained, "F2", true, !_valueJSON.constrained);
+            _valueJSON.slider = _ui.slider;
+
+            _uiTransform.Translate(Vector3.down * 0.3f, Space.Self);
+            _uiTransform.Translate(Vector3.right * 0.35f, Space.Self);
         }
         catch (Exception exc)
         {
-            SuperController.LogError("UISlider Enable: " + exc);
+            SuperController.LogError($"{nameof(UISlider)}.{nameof(OnEnable)}: " + exc);
         }
-    }
-
-    private void CreateUISliderInCanvas()
-    {
-        var canvas = _atom.GetComponentInChildren<Canvas>();
-        if (canvas == null) throw new NullReferenceException("Could not find a canvas to attach to");
-
-        _sliderTransform = Instantiate(manager.configurableSliderPrefab.transform);
-        if (_sliderTransform == null) throw new NullReferenceException("Could not instantiate configurableSliderPrefab");
-        _sliderTransform.SetParent(canvas.transform, false);
-        _sliderTransform.gameObject.SetActive(true);
-
-        _sliderUI = _sliderTransform.GetComponent<UIDynamicSlider>();
-        if (_sliderUI == null) throw new NullReferenceException("Could not find a UIDynamicSlider component");
-        _sliderUI.Configure(_labelJSON.val, _valueJSON.min, _valueJSON.max, _valueJSON.val, _valueJSON.constrained, "F2", true, !_valueJSON.constrained);
-        _valueJSON.slider = _sliderUI.slider;
-
-        _sliderTransform.Translate(Vector3.down * 0.3f, Space.Self);
-        _sliderTransform.Translate(Vector3.right * 0.35f, Space.Self);
     }
 
     public void OnDisable()
     {
-        if (_sliderTransform == null) return;
+        if (_uiTransform == null) return;
 
         try
         {
             _valueJSON.slider = null;
-            Destroy(_sliderTransform.gameObject);
-            _sliderTransform = null;
-            _sliderUI = null;
+            Destroy(_uiTransform.gameObject);
+            _uiTransform = null;
+            _ui = null;
         }
         catch (Exception exc)
         {
-            SuperController.LogError("UISlider Disable: " + exc);
+            SuperController.LogError($"{nameof(UISlider)}.{nameof(OnDisable)}: " + exc);
         }
     }
 
@@ -242,11 +230,5 @@ public class UISlider : MVRScript
         var input = textfield.gameObject.AddComponent<InputField>();
         input.textComponent = textfield.UItext;
         jss.inputField = input;
-    }
-
-    private Exception LogError(Exception exception)
-    {
-        SuperController.LogError($"UISlider: {exception}");
-        return exception;
     }
 }
